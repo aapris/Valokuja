4339a3240c5d68ddf9162c5cac7f07e669bfecf2
Aapo Rista <aapo.gitpub@rista.net>
2020-11-28 21:53:51 +0200

    Move functionality to a class

--- mqtt2wled.py
+++ mqtt2wled.py
@@ -4,140 +4,57 @@ import glob
 import json
 import logging
 import os
+import threading
 
 import configparser
 import paho.mqtt.client as mqtt
 import pytz
 import requests
-import threading
-
-TIMER = None
-SIMULATE = False
-AUTOCHANGE = 300
-B2_LAST_STATE = -1
-B2_LAST_PRESSED = None
-B3_LAST_STATE = -1
-B3_LAST_PRESSED = None
-PRESETS1 = []
-CURRENT_PRESET1 = 0
-PRESETS2 = []
-CURRENT_PRESET2 = 0
-WLED_IPS = ['172.24.1.201', '172.24.1.202', '172.24.1.203', '172.24.1.204']
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--log", dest="log", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        default='ERROR', help="Set the logging level")
-    parser.add_argument('-q', '--quiet', action='store_true', help='Never print a char (except on crash)')
-    parser.add_argument('-f', '--format', required=True,
-                        choices=['jsonsensor', 'ruuvi', 'ruuvitag_collector', 'sensornode'],
-                        help='MQTT message format')
-    parser.add_argument("--autochange", type=float, default=300, help="Seconds to change to next default effect")
-    parser.add_argument("--simulate", action='store_true', help="If set, do not send actual HTTP requests to leds")
+    parser.add_argument(
+        "-l",
+        "--log",
+        dest="log",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="ERROR",
+        help="Set the logging level",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="Never print a char (except on crash)",
+    )
+    parser.add_argument(
+        "--autochange",
+        type=float,
+        default=300,
+        help="Seconds to change to next default effect",
+    )
+    parser.add_argument(
+        "--simulate",
+        action="store_true",
+        help="If set, do not send actual HTTP requests to leds",
+    )
     parser.add_argument("--presets", help="Preset directory", required=True)
-    parser.add_argument("--config", help="Configuration file", default="config.ini", nargs='?')
-    parser.add_argument('-t', '--topic', required=True, nargs='+', help='MQTT topics')
-    parser.add_argument("--mqtt_username", help="MQTT user name", nargs='?')
-    parser.add_argument("--mqtt_password", help="MQTT password", nargs='?')
-    parser.add_argument("--mqtt_host", help="MQTT host", nargs='?')
-    parser.add_argument("--mqtt_port", help="MQTT port", nargs='?')
+    parser.add_argument(
+        "--config", help="Configuration file", default="config.ini", nargs="?"
+    )
+    parser.add_argument("-t", "--topic", required=True, nargs="+", help="MQTT topics")
+    parser.add_argument("--mqtt_username", help="MQTT user name", nargs="?")
+    parser.add_argument("--mqtt_password", help="MQTT password", nargs="?")
+    parser.add_argument("--mqtt_host", help="MQTT host", nargs="?")
+    parser.add_argument("--mqtt_port", help="MQTT port", nargs="?")
     args = parser.parse_args()
     if args.log:
-        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-                            level=getattr(logging, args.log))
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            level=getattr(logging, args.log),
+        )
     return args
-
-
-def load_presets(args):
-    global PRESETS1, PRESETS2
-    preset_files = glob.glob(f'{args.presets}/preset-0*.json')
-    for pf in preset_files:
-        with open(pf, 'rt') as f:
-            PRESETS1.append(f.read())
-    preset_files = glob.glob(f'{args.presets}/preset-1*.json')
-    for pf in preset_files:
-        with open(pf, 'rt') as f:
-            PRESETS2.append(f.read())
-    print(len(PRESETS1), len(PRESETS2))
-
-
-def on_connect(client, userdata, flags, rc):
-    logging.info("Connected with result code {}".format(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    for t in client.args.topic:
-        logging.info(f'Subscribe to {t}')
-        client.subscribe(t)
-
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    payload = msg.payload.decode('utf-8')
-    if msg.retain == 1:
-        logging.info("Do not handle retain message {}".format(payload))
-        return
-    logging.debug("{} '{}'".format(msg.topic, payload))
-    handle_jsonsensor(client, userdata, msg, payload)
-
-
-def next_effect():
-    response = send_telegram(f'Next effect', 1)
-
-
-def handle_jsonsensor(client, userdata, msg, payload):
-    """
-    {'sensor': 'button', 'mac': '80:7D:3A:47:6A:F2', 'id': '200101', 'data': {'b1': 0, 'b2': 1, 'b3': 0}}
-    """
-    global B2_LAST_STATE, B2_LAST_PRESSED, B3_LAST_STATE, B3_LAST_PRESSED
-    now = pytz.timezone("Europe/Helsinki").localize(dt.datetime.now(), is_dst=None)
-    msg = json.loads(payload)
-    try:
-        if msg.get('mac') == '80:7D:3A:47:59:BA':
-            values = msg.get('data')
-            b2 = values.get('b2')
-            b3 = values.get('b3')
-            if b2 is not None and b2 != B2_LAST_STATE:
-                now_str = now.strftime('%d.%m. klo %H:%M:%S')
-                if b2 == 1:
-                    B2_LAST_PRESSED = now
-                    response = send_telegram(f'Button pressed {now_str}', 1)
-                B2_LAST_STATE = b2
-            elif b3 is not None and b3 != B3_LAST_STATE:
-                now_str = now.strftime('%d.%m. klo %H:%M:%S')
-                if b3 == 1:
-                    B3_LAST_PRESSED = now
-                    response = send_telegram(f'Button pressed {now_str}', 2)
-                B3_LAST_STATE = b3
-    except Exception as err:
-        print(err)
-
-
-def send_telegram(msg, p):
-    global PRESETS1, CURRENT_PRESET1, PRESETS2, CURRENT_PRESET2, SIMULATE, TIMER, AUTOCHANGE
-    TIMER.cancel()
-    if p == 1:
-        CURRENT_PRESET1 += 1
-        if CURRENT_PRESET1 >= len(PRESETS1):
-            CURRENT_PRESET1 = 0
-        data = PRESETS1[CURRENT_PRESET1]
-        logging.info(f"Preset 1: #{CURRENT_PRESET1}")
-    else:
-        CURRENT_PRESET2 += 1
-        if CURRENT_PRESET2 >= len(PRESETS2):
-            CURRENT_PRESET2 = 0
-        data = PRESETS2[CURRENT_PRESET2]
-        logging.info(f"Preset 2: #{CURRENT_PRESET2}")
-    headers = {"Content-Type": "application/json"}
-    for ip in WLED_IPS:
-        url = f'http://{ip}/json'
-        if SIMULATE is False:
-            res = requests.post(url, data=data, headers=headers, timeout=1)
-            print(f"{url} {res.status_code}")
-        else:
-            print(f"{url} (simulated)")
-    TIMER = threading.Timer(AUTOCHANGE, next_effect)
-    TIMER.start()
 
 
 def get_setting(args, arg, config, section, key, envname, default=None):
@@ -154,45 +71,173 @@ def get_setting(args, arg, config, section, key, envname, default=None):
         return default
 
 
+class WledController:
+    def __init__(self):
+        self.args = get_args()
+        self.presets1 = []
+        self.presets2 = []
+        self.b2_last_state = -1
+        self.b2_last_pressed = None
+        self.b3_last_state = -1
+        self.b3_last_pressed = None
+        self.current_preset1 = 0
+        self.current_preset2 = 0
+        self.wled_ips = ["172.24.1.201", "172.24.1.202", "172.24.1.203", "172.24.1.204"]
+        self.config = configparser.ConfigParser()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.config.read(os.path.join(dir_path, self.args.config))
+        self.mqtt_user = get_setting(
+            self.args,
+            "mqtt_username",
+            self.config,
+            "mqtt",
+            "username",
+            "MQTT_USERNAME",
+            default="",
+        )
+        self.mqtt_pass = get_setting(
+            self.args,
+            "mqtt_password",
+            self.config,
+            "mqtt",
+            "password",
+            "MQTT_PASSWORD",
+            default="",
+        )
+        self.mqtt_host = get_setting(
+            self.args,
+            "mqtt_host",
+            self.config,
+            "mqtt",
+            "host",
+            "MQTT_HOST",
+            default="127.0.0.1",
+        )
+        self.mqtt_port = get_setting(
+            self.args,
+            "mqtt_port",
+            self.config,
+            "mqtt",
+            "port",
+            "MQTT_PORT",
+            default="1883",
+        )
+        # mqtt_topic = get_setting(args, 'mqtt_topic', config, 'mqtt', 'topic', 'MQTT_TOPIC', default='')
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        self.mclient = mqtt.Client()
+        self.mclient.args = self.args
+        if self.mqtt_user != "":
+            self.mclient.username_pw_set(self.mqtt_user, self.mqtt_pass)
+            logging.debug(f"Using MQTT username and password")
+        self.mclient.on_connect = self.on_connect
+        self.mclient.on_message = self.on_message
+        logging.info(f"Connecting to {self.mqtt_host}:{self.mqtt_port}")
+        self.mclient.connect(self.mqtt_host, int(self.mqtt_port), 60)
+        logging.info("Start listening topic(s): {}".format(", ".join(self.args.topic)))
+
+        self.load_presets()
+        self.timer = threading.Timer(self.args.autochange, self.next_effect)
+        self.timer.start()
+
+        try:
+            self.mclient.loop_forever()
+        except KeyboardInterrupt:
+            self.mclient.disconnect()
+            self.timer.cancel()
+            if self.args.quiet is False:
+                print("Good bye")
+
+    def load_presets(self):
+        preset_files = glob.glob(f"{self.args.presets}/preset-0*.json")
+        for pf in preset_files:
+            with open(pf, "rt") as f:
+                self.presets1.append(f.read())
+        preset_files = glob.glob(f"{self.args.presets}/preset-1*.json")
+        for pf in preset_files:
+            with open(pf, "rt") as f:
+                self.presets2.append(f.read())
+        logging.info(
+            "Loaded {} presets #1 and {} presets #2".format(
+                len(self.presets1), len(self.presets2)
+            )
+        )
+
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("Connected with result code {}".format(rc))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        for t in client.args.topic:
+            logging.info(f"Subscribe to {t}")
+            client.subscribe(t)
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode("utf-8")
+        if msg.retain == 1:
+            logging.info("Do not handle retain message {}".format(payload))
+            return
+        logging.debug("{} '{}'".format(msg.topic, payload))
+        self.handle_jsonsensor(client, userdata, msg, payload)
+
+    def next_effect(self):
+        self.send_telegram(1)
+
+    def handle_jsonsensor(self, client, userdata, msg, payload):
+        """
+        {'sensor': 'button', 'mac': '80:7D:3A:47:59:BA', 'id': '200101', 'data': {'b1': 0, 'b2': 1, 'b3': 0}}
+        """
+        now = pytz.timezone("Europe/Helsinki").localize(dt.datetime.now(), is_dst=None)
+        msg = json.loads(payload)
+        try:
+            if msg.get("mac") == "80:7D:3A:47:59:BA":
+                values = msg.get("data")
+                b2 = values.get("b2")
+                b3 = values.get("b3")
+                if b2 is not None and b2 != self.b2_last_state:
+                    if b2 == 1:
+                        self.b2_last_pressed = now
+                        self.send_telegram(1)
+                    self.b2_last_state = b2
+                elif b3 is not None and b3 != self.b3_last_state:
+                    if b3 == 1:
+                        self.b3_last_pressed = now
+                        self.send_telegram(2)
+                    self.b3_last_state = b3
+        except Exception as err:
+            print(err)
+
+    def send_telegram(self, p):
+        self.timer.cancel()
+        if p == 1:
+            self.current_preset1 += 1
+            if self.current_preset1 >= len(self.presets1):
+                self.current_preset1 = 0
+            data = self.presets1[self.current_preset1]
+            logging.info(f"Preset 1: #{self.current_preset1}")
+        else:
+            self.current_preset2 += 1
+            if self.current_preset2 >= len(self.presets2):
+                self.current_preset2 = 0
+            data = self.presets2[self.current_preset2]
+            logging.info(f"Preset 2: #{self.current_preset2}")
+        headers = {"Content-Type": "application/json"}
+        for ip in self.wled_ips:
+            url = f"http://{ip}/json"
+            if self.args.simulate is False:
+                res = requests.post(url, data=data, headers=headers, timeout=1)
+                print(f"{url} {res.status_code}")
+            else:
+                print(f"{url} (simulated)")
+        self.timer = threading.Timer(self.args.autochange, self.next_effect)
+        self.timer.start()
+
+
 def main():
-    global SIMULATE, TIMER, AUTOCHANGE
-    args = get_args()
-    AUTOCHANGE = args.autochange
-    if args.simulate:
-        SIMULATE = True
-    config = configparser.ConfigParser()
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    config.read(os.path.join(dir_path, args.config))
-    mqtt_user = get_setting(args, 'mqtt_username', config, 'mqtt', 'username', 'MQTT_USERNAME', default='')
-    mqtt_pass = get_setting(args, 'mqtt_password', config, 'mqtt', 'password', 'MQTT_PASSWORD', default='')
-    mqtt_host = get_setting(args, 'mqtt_host', config, 'mqtt', 'host', 'MQTT_HOST', default='127.0.0.1')
-    mqtt_port = get_setting(args, 'mqtt_port', config, 'mqtt', 'port', 'MQTT_PORT', default='1883')
-    # mqtt_topic = get_setting(args, 'mqtt_topic', config, 'mqtt', 'topic', 'MQTT_TOPIC', default='')
-    load_presets(args)
-    # Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
-    mclient = mqtt.Client()
-    mclient.args = args
-    if mqtt_user != '':
-        mclient.username_pw_set(mqtt_user, mqtt_pass)
-        logging.debug(f'Using MQTT username and password')
-    mclient.on_connect = on_connect
-    mclient.on_message = on_message
-    logging.info(f'Connecting to {mqtt_host}:{mqtt_port}')
-    mclient.connect(mqtt_host, int(mqtt_port), 60)
-    logging.info('Start listening topic(s): {}'.format(', '.join(args.topic)))
-    try:
-        TIMER = threading.Timer(AUTOCHANGE, next_effect)
-        TIMER.start()
-        mclient.loop_forever()
-    except KeyboardInterrupt:
-        mclient.disconnect()
-        TIMER.cancel()
-        if args.quiet is False:
-            print("Good bye")
+    wc = WledController()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
